78f2d7cc2edcc5354f821a99155eb0e0a86a870f
Musfiq Rahman Saikat <79831764+musfiq180214@users.noreply.github.com>
2025-09-13 14:19:47 +0600

    Delete Office_management directory

--- Office_management/lib/core/constants.dart
+++
@@ -1,9 +0,0 @@
-class EndPoints {
-  static const String baseUrl = 'http://internal-api.misfit.tech';
-  static const String attendencesPath = '/emp/api/v1/attendances';
-  static const String avgAttendecPath = '/emp/api/v1/average';
-  static const String loginPath = '/emp/api/v1/login';
-  static const String dashboarPath = '/emp/api/v1/dashboard';
-  static const String employeeListPath = '/emp/api/v1/list';
-  static const String signinpage = '/signin';
-}
